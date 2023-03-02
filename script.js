292a4a0987188b7905a5971d905f3f3dd224554a
benjaquemero <benja.vallina@gmail.com>
2023-03-02 00:07:14 -0300

    se agrega el proyecto

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("boton").onclick = function(){
+    document.getElementById("vacio").innerHTML="Futuro Desarrollador Web"
+}
+
+document.getElementById("ocultar").onclick = function(){
+    document.getElementById("vacio").innerHTML=""
+}
